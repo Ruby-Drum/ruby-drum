ff1c02c2689cfcacaced0f3ac61789c103b9eba0
Mike Bateman <bateman.118@buckeyemail.osu.edu>
2024-03-22 16:38:07 -0400

    Add Navigation Bar

---
+++ src/web/src/Pages/MyOrder/index.tsx
@@ -0,0 +1,9 @@
+const MyOrder: React.FC = () => {
+    return (
+        <div>
+                <h1>My Order</h1>
+        </div>
+    );
+};
+
+export default MyOrder;
---
+++ src/web/src/Pages/MyOrders/index.tsx
@@ -0,0 +1,9 @@
+const MyOrders: React.FC = () => {
+    return (
+        <div>
+                <h1>My Orders</h1>
+        </div>
+    );
+};
+
+export default MyOrders;
