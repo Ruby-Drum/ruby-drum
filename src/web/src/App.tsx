15e79d7e5c2c2ad932dae8fd0e18e6da05caf628
Camhack1 <hacker.106@buckeyemail.osu.edu>
2024-03-22 11:23:38 -0400

    .

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+
 function App() {
   return(
     <>
-      <h1>Hello, World!</h1>
+      <div className='bg-gray-500'>Hello, World!</div>
     </>
   )
-
+  
 }
 
 export default App;
