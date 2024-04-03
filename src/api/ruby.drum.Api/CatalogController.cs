3be1d6cf57454da1358413aca24c23a0ddb65ef8
Camhack1 <hacker.106@buckeyemail.osu.edu>
2024-04-03 12:12:08 -0400

    Adding Catalog API- Get Item by ID

--- src/api/ruby.drum.Api/CatalogController.cs
+++ src/api/ruby.drum.Api/CatalogController.cs
@@ -23,5 +23,40 @@ namespace Ruby.Drum.Api.Controllers
 
             return Ok(items);
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Item 1", "Description 1", "Brand 1", 100.00m)
+            {
+                Id = id
+            };
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateItem(Item item)
+        {
+            return CreatedAtAction(nameof(GetItem), new { id = 42 }, item);
+        }
+
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult AddRating(int id, Rating rating)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateItem(int id, Item item)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteItem(int id)
+        {
+            return NoContent();
+        }
     }
 }
--- src/api/ruby.drum.Domain/Catalog/Item.cs
+++ src/api/ruby.drum.Domain/Catalog/Item.cs
@@ -2,7 +2,7 @@ namespace Ruby.Drum.Domain.Catalog;
 
 public class Item
 {   
-    public int ID { get; set;}
+    public int Id { get; set;}
     public string Name { get; set;}
     public string Description { get; set;}
     public string Brand { get; set;}
