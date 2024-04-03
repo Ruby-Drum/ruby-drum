49a6c55941f91b5cdea8b174a6a72f3d1c721019
Camhack1 <hacker.106@buckeyemail.osu.edu>
2024-04-03 11:55:58 -0400

    Adding Swagger UI

--- src/api/ruby.drum.Domain/Catalog/Item.cs
+++ src/api/ruby.drum.Domain/Catalog/Item.cs
@@ -5,7 +5,7 @@ public class Item
     public int ID { get; set;}
     public string Name { get; set;}
     public string Description { get; set;}
-    public required string Brand { get; set;}
+    public string Brand { get; set;}
     public decimal Price { get; set;}
     public List<Rating> Ratings { get; set; } = new List<Rating>();
 
