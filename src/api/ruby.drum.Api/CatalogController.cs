2216923ada8c58978f892d0c85ef21a91a1b62bb
Mike Bateman <bateman.118@buckeyemail.osu.edu>
2024-04-03 22:14:05 -0400

    adding post rating

--- src/api/ruby.drum.Api/CatalogController.cs
+++ src/api/ruby.drum.Api/CatalogController.cs
@@ -55,7 +55,15 @@ namespace ruby.drum.Api.Controllers
         [HttpPost("{id:int}/ratings")]
         public IActionResult AddRating(int id, Rating rating)
         {
-            return Ok();
+            var item = _context.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            item.AddRating(rating);
+            _context.SaveChanges();
+
+            return Ok(item);
         }
 
         [HttpPut("{id:int}")]
