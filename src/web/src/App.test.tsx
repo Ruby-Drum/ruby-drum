ad4a171d4251b9e74169868b13ca1382eb263a12
Camhack1 <hacker.106@buckeyemail.osu.edu>
2024-04-18 13:42:27 -0400

    App tests

---
+++ src/web/src/App.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe("App component tests", () => {
+    test("should show title all the time", () => {
+
+        render(<App/>)
+
+        expect(screen.getByText(/Ruby Drum/i))
+        .toBeDefined()
+    })
+
+    test("should show 'All' link in the menu", () => {
+
+        render(<App/>)
+
+        expect(screen.getByRole('link', {name: /All/i}))
+    })
+
+    test("should show 'Home' heading in 'H1' tag", () => {
+
+        render(<App/>)
+
+        expect(screen.getByRole('heading', {level: 1, name: /Home/i}))
+        .toBeDefined()
+    })
+})
